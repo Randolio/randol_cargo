b61725d2857bff2fe5e23d13f7c6240aa7f35054
Andy <86536434+Andyyy7666@users.noreply.github.com>
2024-04-01 23:22:46 +0200

    Update nd.lua

--- bridge/server/nd.lua
+++ bridge/server/nd.lua
@@ -1,17 +1,16 @@
 if not lib.checkDependency('ND_Core', '2.0.0') then return end
 
-NDCore = {}
-
-lib.load('@ND_Core.init')
+NDCore = exports["ND_Core"]
 
 function GetPlayer(id)
-    return NDCore.getPlayer(id)
+    return NDCore:getPlayer(id)
 end
 
 function DoNotification(src, text, nType)
-    TriggerClientEvent('ox_lib:notify', src, { type = nType, description = text })
+    local player = NDCore:getPlayer(src)
+    return player and player.notify({ type = nType, description = text })
 end
 
-function AddMoney(Player, moneyType, amount)
-    Player.addMoney(moneyType, amount, "cargo-delivery")
+function AddMoney(player, moneyType, amount)
+    player.addMoney(moneyType, amount, "cargo-delivery")
 end
