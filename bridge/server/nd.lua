7a9e1c5a0816f59af5ea9c22d716497cd7986cc9
Andy <86536434+Andyyy7666@users.noreply.github.com>
2024-04-01 23:23:36 +0200

    Update nd.lua

--- bridge/server/nd.lua
+++ bridge/server/nd.lua
@@ -1,6 +1,6 @@
 if not lib.checkDependency('ND_Core', '2.0.0') then return end
 
-NDCore = exports["ND_Core"]
+local NDCore = exports["ND_Core"]
 
 function GetPlayer(id)
     return NDCore:getPlayer(id)
